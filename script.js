3f2f122b7a4603b0c6d27a82a0e852e2528394ab
Excellent Emmanuel <emmanuecreativeco@gmail.com>
2025-06-04 05:37:26 -1200

    Update Web Page And Sections

--- script.js
+++ script.js
@@ -60,28 +60,3 @@ function handleSubmit(e) {
       submitBtn.innerText = "Send Message";
     });
 }
-document.addEventListener("DOMContentLoaded", function () {
-  const faqItems = document.querySelectorAll(".faq-item");
-
-  faqItems.forEach((item) => {
-    const question = item.querySelector(".faq-question");
-    const answer = item.querySelector(".faq-answer");
-
-    question.addEventListener("click", () => {
-      const isOpen = item.classList.contains("open");
-
-      // Close all
-      faqItems.forEach((i) => {
-        i.classList.remove("open");
-        const iAnswer = i.querySelector(".faq-answer");
-        if (iAnswer) iAnswer.classList.remove("open");
-      });
-
-      // Toggle current
-      if (!isOpen) {
-        item.classList.add("open");
-        answer.classList.add("open");
-      }
-    });
-  });
-});
