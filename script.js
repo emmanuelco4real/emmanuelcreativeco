df8ccad2eea7ca55332caac31de4ca5850ae468a
Excellent Emmanuel <emmanuecreativeco@gmail.com>
2025-06-07 01:45:28 -1200

    Final Web Update june 07 2025 1:45 pm

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-window.addEventListener("load", function () {
-  const preloader = document.getElementById("preloader");
-  preloader.style.opacity = "0";
-  preloader.style.visibility = "hidden";
-});
 function showSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "flex";
